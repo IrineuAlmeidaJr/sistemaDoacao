6db039f2db0ebeb59ba349cb2d51a7a96633e601
irickdias <henriquedias180@gmail.com>
2022-04-22 11:07:12 -0300

    formulario cadastro de doacao
    formulario cadastro de doacao criada

--- site/frontend/src/pages/CadDoacao.js
+++ site/frontend/src/pages/CadDoacao.js
@@ -1,12 +1,24 @@
-import {useState} from 'react';
+import React from 'react';
 
 export default function FormCadDoacao(props){
     return(
         <div>
             <form>
                 {/* tipo da doacao - brinquedo, roupa etc - combo box */}
+                <label for="tipoDoacao">Tipo da doação:</label>
+                <select>
+                    <option value="Brinquedo">Brinquedo</option>
+                    <option value="Roupas">Roupas</option>
+                    <option value="Outros">Outros</option>
+                </select>
+
                 {/* quantidade - input text*/}
+                <label for="qtdeDoacao">Quantidade:</label>
+                <input type="text" id="qtdeDoacao" name="qtdeDoacao"/>
+
                 {/* descricao da doacao - input text*/}
+                <label for="descDoacao">Descrição:</label>
+                <input type="text" id="descDoacao" name="descDoacao"/>
             </form>
             
         </div>
--- site/frontend/src/routes.js
+++ site/frontend/src/routes.js
@@ -4,13 +4,15 @@ import { Route, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import ListaUsuario from "./pages/ListaUsuario";
 import CadUsuario from "./pages/CadUsuario";
+import FormCadDoacao from "./pages/CadDoacao";
 
 const Routes = () => {
    return(
        <BrowserRouter>
            <Route component = { Home }  path="/" exact />
            <Route component = { CadUsuario }  path="/cadUsuario" />
-           <Route component = { ListaUsuario }  path="/listaUsuario" />           
+           <Route component = { ListaUsuario }  path="/listaUsuario" />
+           <Route component = { FormCadDoacao }  path="/cadDoacao" />              
        </BrowserRouter>
    )
 }
