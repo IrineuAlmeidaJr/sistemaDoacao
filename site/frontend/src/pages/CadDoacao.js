6eb2721f229b1ea7fdab4a2b0399dd30b9b863b0
irickdias <henriquedias180@gmail.com>
2022-04-22 16:38:40 -0300

    mudanças
    pasta css criada
    cadDoação alterada

--- site/frontend/src/pages/CadDoacao.js
+++ site/frontend/src/pages/CadDoacao.js
@@ -1,26 +1,41 @@
 import React from 'react';
+import Header from '../components/Header';
+import "../css/Formularios.css";
+import "../css/Gerais.css";
 
 export default function FormCadDoacao(props){
     return(
+    
         <div>
-            <form>
-                {/* tipo da doacao - brinquedo, roupa etc - combo box */}
-                <label for="tipoDoacao">Tipo da doação:</label>
-                <select>
-                    <option value="Brinquedo">Brinquedo</option>
-                    <option value="Roupas">Roupas</option>
-                    <option value="Outros">Outros</option>
-                </select>
+            <Header/>
+            <div class="formBox">
+                <form>
+                    <h3 class="titulo-style-1">Cadastrar Doação</h3><br/>
+                    {/* tipo da doacao - brinquedo, roupa etc - combo box */}
+                    <label class="label-bold" for="tipoDoacao">Tipo da doação:</label><br/>
+                    <select class="cb-tipoDoacao" >
+                        <option value="Brinquedo">Brinquedo</option>
+                        <option value="Roupas">Roupas</option>
+                        <option value="Outros">Outros</option>
+                    </select>
 
-                {/* quantidade - input text*/}
-                <label for="qtdeDoacao">Quantidade:</label>
-                <input type="text" id="qtdeDoacao" name="qtdeDoacao"/>
+                    <br/><br/>
 
-                {/* descricao da doacao - input text*/}
-                <label for="descDoacao">Descrição:</label>
-                <input type="text" id="descDoacao" name="descDoacao"/>
-            </form>
-            
+                    {/* quantidade - input text*/}
+                    <label class="label-bold" for="qtdeDoacao">Quantidade:</label><br/>
+                    <input class="input-style-1" type="text" id="qtdeDoacao" name="qtdeDoacao" size="15"/>
+
+                    <br/><br/>
+
+                    {/* descricao da doacao - input text*/}
+                    <label class="label-bold" for="descDoacao">Descrição:</label><br/>
+                    {/*<input class="input-style-1" type="text" id="descDoacao" name="descDoacao"/>
+                    <br/><br/>*/}
+                    <textarea class="ta-descricao" id="descDoacao" name="descDoacao"></textarea>
+                    <br/><br/>
+                    <button class="button-doacao" type="submit">Confirmar</button>
+                </form>
+            </div>
         </div>
     );
 }
