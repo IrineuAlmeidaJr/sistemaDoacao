f59da9e97b65eca2f4ffcde18a594329ad657e01
irickdias <henriquedias180@gmail.com>
2022-04-22 00:03:27 -0300

    Update CadDoacao.js

--- site/frontend/src/pages/CadDoacao.js
+++ site/frontend/src/pages/CadDoacao.js
@@ -3,9 +3,12 @@ import {useState} from 'react';
 export default function FormCadDoacao(props){
     return(
         <div>
-            {/* tipo da doacao - brinquedo, roupa etc - combo box */}
-            {/* quantidade - input text*/}
-            {/* descricao da doacao - input text*/}
+            <form>
+                {/* tipo da doacao - brinquedo, roupa etc - combo box */}
+                {/* quantidade - input text*/}
+                {/* descricao da doacao - input text*/}
+            </form>
+            
         </div>
     );
 }
