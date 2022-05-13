8fb3ec80e83ac1e5d8924675a51f94f0069802d6
Negan59 <guilherme.oliveira@unesp.br>
2022-05-12 23:48:36 -0300

    funcao basica tamanho com mvc 100%

--- site/backend/controller/tamanhocontroller.js
+++ site/backend/controller/tamanhocontroller.js
@@ -4,7 +4,7 @@ module.exports = {
     async gravar(req,res){
         const tam = { ...req.body }
         const con = await db.conecta()
-        let novo = new tamanho(tam.cod,tam.nome)
+        let novo = new tamanho(null,tam.nome)
         await novo.gravar(db)
         return res.json(novo)
     },
